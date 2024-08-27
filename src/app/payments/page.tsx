ab1b2d3d99be13cafe5ee42d66057d5507eae0ef
Natnatee <natnatee_mond@hotmail.com>
2024-08-27 10:44:40 +0700

    s

--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -1,17 +1,26 @@
 "use client";
 import Navbar from "@/components/Navbar";
-import { CartProvider } from "@/context/CartContext";
+import { useCart } from "@/context/CartContext";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Payments: React.FC = () => {
 	const [qrCodeURI, setQrCodeURI] = useState<string | null>(null);
+	const { order } = useCart(); // useCart เพื่อเข้าถึง Context
 
 	useEffect(() => {
 		const fetchQrCode = async () => {
 			try {
+				let total: number = 0;
+				order.forEach((item) => {
+					total += item.price * item.quantity; // ใช้ item.price และคูณกับจำนวนที่สั่งซื้อ
+				});
+
+				console.log("Calculated Total:", total);
+				total = total * 100;
+				// Make the API call with the calculated total directly
 				const response = await axios.post("/api/create_qr_code", {
-					amount: 10000,
+					amount: total,
 					description: "555",
 				});
 
@@ -19,17 +28,24 @@ const Payments: React.FC = () => {
 					setQrCodeURI(response.data.qrCodeURI);
 				}
 			} catch (error) {
-				console.log("Fail to create QR Code");
+				console.error("Failed to create QR Code:", error);
 			}
 		};
-		fetchQrCode();
-	}, []);
+
+		// Call fetchQrCode only if the order has items
+		if (order.length > 0) {
+			fetchQrCode();
+		}
+	}, [order]); // Add `order` as a dependency
 
 	return (
-		<CartProvider>
+		<>
 			<Navbar />
 			<section className=" bg-green-600 flex justify-center ">
 				<div className="bg-orange-600 w-[1440px] min-h-[1440px]">
+					<h1 className="bg-blue-400 w-full min-h-[300px] flex items-center justify-center text-6xl">
+						อย่าชำระเงินจริง นี่เป็นแค่ตัวอย่าง !!!
+					</h1>
 					<div className=" bg-white flex justify-center">
 						<div className="bg-orange-400 aspect-[1/1] min-w-[500px] w-1/2 text-center content-center">
 							{qrCodeURI ? (
@@ -46,7 +62,7 @@ const Payments: React.FC = () => {
 					<div className=" bg-white "></div>
 				</div>
 			</section>
-		</CartProvider>
+		</>
 	);
 };
 
